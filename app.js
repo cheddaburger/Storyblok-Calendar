d2cb56b5ad7267b0953741e7cdb7e5ec0b8ca1a1
cheddaburger <s.minemann@gmail.com>
2024-06-16 20:36:33 -0500

    new commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const calendar = document.getElementById('calendar');
+const monthEl = document.getElementById('month');
 
 
 const months = [
     'January',
-    'Febuary',
+    'February',
     'March',
     'April',
     'May',
@@ -13,18 +14,23 @@ const months = [
     'September',
     'October',
     'November',
-    'December'
+    'December',
 ];
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
+const today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+
 const drawBlankCalendar = () => {
-    for( let i = 0; i <35; i++) {
+    for(let i = 0; i < 35; i++) {
         const day = document.createElement('div');
         day.classList.add('day');
 
         const dayText = document.createElement('p');
         dayText.classList.add('day-text');
+        dayText.innerText = days[i % 7];
 
         const dayNumber = document.createElement('p');
         dayNumber.classList.add('day-number');
@@ -41,20 +47,49 @@ const drawBlankCalendar = () => {
 };
 
 const updateCalendar = (month, year, events) => {
-    const dayElements = document.querySelectorALl('.day');
+    const dayElements = document.querySelectorAll('.day');
 
     const theFirst = new Date();
-    theFirst.setMonth(month)
+    theFirst.setMonth(month);
     theFirst.setYear(year);
 
     const theFirstDayOfWeek = theFirst.getDay();
     const monthName = months[month];
-    const monthWithYear = `${year} - ${month}`;
+    const monthWithYear = `${year} - ${monthName}`;
+    monthEl.innerText = monthWithYear;
     const daysInMonth = new Date(year, month + 1, 0).getDate();
-
-    for(let i = 0; i< dayElements.length; i++){
+    
+    let dayCounter = 1; 
+    for (let i = 0; i < dayElements.length; i++) {
         const day = dayElements[i];
-        const dayText = day.querySelector('.day-text')
-    }
+        
+
+        const dayNumber = day.querySelector('.day-number');
+        if( i >= theFirstDayOfWeek && dayCounter <= daysInMonth) {
+            dayNumber.innerText = dayCounter;
+            dayCounter++;
+        } else {
+          dayNumber.innerText = '';
+        }
 }
+        
+};
+
+const previousMonth = () => {
+    if (currentMonth === 0) {
+        currentMonth = 12;
+        currentYear--;
+    }
+ updateCalendar(--currentMonth, currentYear);
+};
+
+const nextMonth = () => {
+ updateCalendar(++currentMonth, currentYear);
+ if (currentMonth === 11) {
+    currentMonth = -1;
+    currentYear++;
+}
+};
+ 
 drawBlankCalendar();
+updateCalendar(currentMonth, currentYear); 
